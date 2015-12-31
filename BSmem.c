7e68ab01d8da81cb9f26143c9629e17f2642f709
Eegger <2386881494@qq.com>
2015-12-31 08:30:16 +0800

    一次分配页面和写入内容，读取60遍，每次输出开始出错的内存其实地址，十六进制输出

--- BSmem.c
+++ BSmem.c
@@ -9,11 +9,13 @@ ReadAfterAllocateAndWrite(){
 
 	EFI_STATUS Status = 0;
 
-	EFI_PHYSICAL_ADDRESS pages = 1157627904;//0x45000000
-	int NumberOfPage = 0;
+	//EFI_PHYSICAL_ADDRESS pages = 1157627904;//0x45000000
+    EFI_PHYSICAL_ADDRESS pages = 6442450944;//赋初始值为0x180000000，表示要操作内存的起始物理地址。
+	//int NumberOfPage = 0;
 	int j = 0;
 	//allocate pages and write into all the pages
-    while(NumberOfPage<10){
+    //while(NumberOfPage<10){
+    while(pages<10737418240){//0x280000000，表示物理地址的最大范围。   
 
 		//Allocate Address
     	Status = gBS->AllocatePages(AllocateAddress, EfiBootServicesData, 1, &pages);
@@ -30,7 +32,7 @@ ReadAfterAllocateAndWrite(){
             }
 
             //Status = gBS->FreePages(pages,1);
-            Print(L"Test Str:%c\n", str[0]);
+            //Print(L"Test Str:%c\n", str[0]);
             //if not Free the page,allocate pages will fail!
             //Status = gBS->FreePages(pages,1);
     	}
@@ -39,38 +41,66 @@ ReadAfterAllocateAndWrite(){
     	}
 
     	pages+=4096;
-    	NumberOfPage++;
+    	//NumberOfPage++;
     }
 
+    Print(L"Allocate and write finished!\n");
     //allocate pages and test
-    int NumberOfTest = 0;
+    //int NumberOfTest = 0;
     int k = 0;
     int ReadWrongNumber = 0;
 
-    pages = 1157627904;//0x45000000
+    
 
-    while(NumberOfTest<10){
+    int readTimes = 1;
+    EFI_PHYSICAL_ADDRESS ResultAddress = 0;
+    //while(NumberOfTest<10){
+    while(readTimes<=6){
 
-        if(Status == 0){
-            CHAR8* str;
-            str = (CHAR8*)pages;
-            k = 0;
-            while(k<4096){
-                if(str[k] != ('A'+k%26)){
-                    ReadWrongNumber++;
+        pages = 6442450944;//赋初始值为0x180000000，表示要内存的起始物理地址。
+
+        while(pages<10737418240){//0x280000000，表示物理地址的最大范围。  
+
+            if(Status == 0){
+                CHAR8* str;
+                str = (CHAR8*)pages;
+                k = 0;
+                while(k<4096){
+                    if(str[k] != ('A'+k%26)){
+                        ReadWrongNumber++;
+                        ResultAddress = pages+k+1;
+                        break;
+                    }
+                    // else{
+                    //     Print(L"Read Test Str:%c\n", str[1]);
+                    // }
+                    k++;
                 }
-                // else{
-                //     Print(L"Read Test Str:%c\n", str[1]);
-                // }
-                k++;
-            }
 
-            Status = gBS->FreePages(pages,1);
+           // Status = gBS->FreePages(pages,1);
+            }
+            pages+=4096;
+            // NumberOfTest++;
         }
+
+
+        Print(L"read %d times\n",readTimes);
+        readTimes++;
+
+        if (ReadWrongNumber!=0)
+        {
+            break;
+        }
+    }
+    
+    pages = 6442450944;//赋初始值为0x180000000，表示要内存的起始物理地址。
+    while(pages<10737418240){
+
+        Status = gBS->FreePages(pages,1);
         pages+=4096;
-        NumberOfTest++;
     }
 
+    //FreePages 
     // while(NumberOfTest<10){
 
     // 	Status = gBS->AllocatePages(AllocateAddress,EfiBootServicesData,1,&pagesForTest);
@@ -102,13 +132,15 @@ ReadAfterAllocateAndWrite(){
     // }
 
     
+    //Print(L"After Read %d times!\n",(readTimes-1));
 
-    Print(L"\nTest Result is Following:\n");
+    Print(L"Test Result is Following:\n");
     if(ReadWrongNumber == 0){
-    	Print(L"Write and Read is total Same");
+    	Print(L"Amazing!everything is right!");
     }
     else{
-    	Print(L"Different times is %d\n",ReadWrongNumber);
+        Print(L"ResultAddress %lx\n",ResultAddress);
+    	Print(L"The Different times is %d\n",ReadWrongNumber);
     }
     
 	return Status;
